05919195de48dc8ec6ae519c596005da764a8ef8
Fernando Soares <fernando.soares@luizalabs.com>
2020-02-29 12:02:28 -0300

    :sparkles: Add fuel calculate

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 import {inject, observer} from 'mobx-react';
+import {Button, Card, Input, Text} from '@ui-kitten/components';
+
+import {StyleSheet} from 'react-native';
 
 import HomeStore from '../../stores/home.store';
 
@@ -12,13 +14,46 @@ interface Props {
 @observer
 export default class Home extends Component<Props> {
   render() {
+    const {
+      ethanol,
+      gasoline,
+      result,
+      calculate,
+      handleForm,
+    } = this.props.homeStore;
+
     return (
       <>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Home Screen</Text>
-          <Text>{this.props.homeStore.numbers}</Text>
-        </View>
+        <Card>
+          <Text>Etanol:</Text>
+          <Input
+            value={ethanol.toString()}
+            onChangeText={ethanol => handleForm({ethanol})}
+          />
+          <Text>Gasolina:</Text>
+          <Input
+            value={gasoline.toString()}
+            onChangeText={gasoline => handleForm({gasoline})}
+          />
+          <Button onPress={() => calculate()}>Calcular</Button>
+          <Text style={styles.paragraph}>{result}</Text>
+        </Card>
       </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: '10',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- src/stores/home.store.tsx
+++ src/stores/home.store.tsx
@@ -1,14 +1,32 @@
 import {action, observable} from 'mobx';
 
 export default class HomeStore {
-  @observable numbers = 0;
+  @observable ethanol = 0;
+  @observable gasoline = 0;
+  @observable result = '';
 
-  @action reset = () => {
-    this.numbers = 0;
+  @action calculate = () => {
+    const {ethanol, gasoline} = this;
+    const ethanolNumber = Number(ethanol);
+    const gasolineNumber = Number(gasoline);
+
+    if (!isNaN(ethanolNumber) && !isNaN(gasolineNumber)) {
+      const result = ethanolNumber / gasolineNumber;
+
+      if (result > 0.7) {
+        this.result = 'Vale a pena gasolina';
+      } else if (result < 0.7) {
+        this.result = 'Vale a pena etanol';
+      } else {
+        this.result = 'São equivalentes';
+      }
+    }
   };
 
-  @action increment = () => {
-    this.numbers += 1;
+  @action handleForm = input => {
+    const key = Object.keys(input)[0];
+    const value = input[key];
+    this[key] = value;
   };
 }
 
