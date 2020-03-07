df3a1d4fb8a8ea3a7976a08efa415755def338de
Fernando Soares <fernando.soares@luizalabs.com>
2020-03-07 09:45:43 -0300

    :lipstick: Update styles

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
-import {Button, Card, Input, Text} from '@ui-kitten/components';
-
 import {StyleSheet} from 'react-native';
+import {
+  Layout,
+  Card,
+  CardHeader,
+  Button,
+  Input,
+  Text,
+  Modal,
+} from '@ui-kitten/components';
 
 import HomeStore from '../../stores/home.store';
 
@@ -10,34 +17,66 @@ interface Props {
   homeStore: HomeStore;
 }
 
+const Header = () => (
+  <CardHeader title="Informe os valores dos combustíveis atualmente" />
+);
+
 @inject('homeStore')
 @observer
 export default class Home extends Component<Props> {
+  state = {
+    modalVisible: false,
+  };
+
+  closeModal = () => {
+    this.setState({modalVisible: false});
+  };
+
+  openModal = () => {
+    const {calculate} = this.props.homeStore;
+
+    /**
+     * Calculate the result
+     */
+    calculate();
+
+    this.setState({modalVisible: true});
+  };
+
+  renderModalElement = result => (
+    <Layout level="3" style={styles.modalContainer}>
+      <Text style={styles.paragraph}>{result}</Text>
+      <Button onPress={() => this.closeModal()}>Fechar</Button>
+    </Layout>
+  );
   render() {
-    const {
-      ethanol,
-      gasoline,
-      result,
-      calculate,
-      handleForm,
-    } = this.props.homeStore;
+    const {ethanol, gasoline, result, handleForm} = this.props.homeStore;
 
     return (
       <>
-        <Card>
-          <Text>Etanol:</Text>
-          <Input
-            value={ethanol.toString()}
-            onChangeText={ethanol => handleForm({ethanol})}
-          />
-          <Text>Gasolina:</Text>
-          <Input
-            value={gasoline.toString()}
-            onChangeText={gasoline => handleForm({gasoline})}
-          />
-          <Button onPress={() => calculate()}>Calcular</Button>
-          <Text style={styles.paragraph}>{result}</Text>
-        </Card>
+        <Layout style={styles.container}>
+          <Card header={Header} status="success">
+            <Text>Etanol:</Text>
+            <Input
+              value={ethanol.toString()}
+              onChangeText={ethanol => handleForm({ethanol})}
+            />
+            <Text>Gasolina:</Text>
+            <Input
+              value={gasoline.toString()}
+              onChangeText={gasoline => handleForm({gasoline})}
+            />
+            <Button onPress={() => this.openModal()} style={styles.button}>
+              Calcular
+            </Button>
+          </Card>
+        </Layout>
+        <Modal
+          backdropStyle={styles.backDrop}
+          onBackdropPress={() => this.closeModal()}
+          visible={this.state.modalVisible}>
+          {this.renderModalElement(result)}
+        </Modal>
       </>
     );
   }
@@ -45,15 +84,29 @@ export default class Home extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: '10',
-    padding: 8,
+    display: 'flex',
+    paddingVertical: 125,
+    paddingLeft: 15,
+    paddingRight: 15,
+    minHeight: '100%',
+    backgroundColor: '#2b7cd7',
+  },
+  button: {
+    marginTop: 25,
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 256,
+    padding: 16,
+  },
+  backDrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
 });
